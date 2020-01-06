7dce57f7bedf224f8375a3a464f4caef1912f1f4
Baizey <lassemoerk.mrk@gmail.com>
2020-01-06 09:47:26 +0100

    .

--- AndrewKennedyTranslation.fs
+++ AndrewKennedyTranslation.fs
@@ -9,44 +9,38 @@ module AndrewKennedyTree =
 
     type Extent = (float * float) list
 
-    let moveTree (tree: Tree, x: float): Tree =
+    let moveTree (tree: Tree, x: float) =
         let (label, at) = tree.node
 
         let newTree =
-            { node = Node(label, at + x)
+            { node = (label, at + x)
               children = tree.children }
         newTree
 
     let moveExtent (extent: Extent, x: float): Extent =
         extent
         |> List.map (fun item ->
-            match item with
-            | (p, q) -> (p + x, q + x))
+            let (p, q) = item
+            (p + x, q + x))
 
-    let rec merge (a: Extent, b: Extent): Extent =
+    let rec merge (a: Extent, b: Extent) =
         match (a, b) with
         | ([], qs) -> qs
         | (ps, []) -> ps
         | ((p, _) :: ps, (_, q) :: qs) -> (p, q) :: merge (ps, qs)
 
-    let rmax (p: float, q: float): float =
+    let rmax (p: float, q: float) =
         if p > q then p
         else q
 
     let mean (x: float, y: float): float = (x + y) / 2.0
 
-    let rec fit (a: Extent, b: Extent): float =
+    let rec fit (a: Extent, b: Extent) =
         match (a, b) with
         | ((_, p) :: ps, (q, _) :: qs) -> rmax (fit (ps, qs), p - q + 1.0)
         | _, _ -> 0.0
 
-    let flipExtent (extent: Extent): Extent =
-        extent
-        |> List.map (fun item ->
-            match item with
-            | (p, q) -> (-q, -p))
-
-    let fitlistLeft (es: Extent list): float list =
+    let fitlistLeft (es: Extent list) =
         let rec fitlist (acc: Extent, es: Extent list) =
             match (acc, es) with
             | (_, []) -> []
@@ -55,7 +49,7 @@ module AndrewKennedyTree =
                 x :: fitlist (merge (acc, moveExtent (e, x)), es)
         fitlist ([], es)
 
-    let fitlistRight (es: Extent list): float list =
+    let fitlistRight (es: Extent list) =
         let rec fitlist (acc: Extent, es: Extent list) =
             match (acc, es) with
             | (_, []) -> []
@@ -64,30 +58,20 @@ module AndrewKennedyTree =
                 x :: fitlist (merge (moveExtent (e, x), acc), es)
         fitlist ([], es)
 
-    let fitlist (es: Extent list): float list =
-        let a = fitlistLeft es
-        let b = fitlistRight es
-        let zipped = List.zip a b
-        List.map mean zipped
+    let fitlist (es: Extent list) =
+        List.map mean (List.zip (fitlistLeft es) (fitlistRight es))
 
     let designTree (tree: Tree) =
         let rec innerDesign (tree: Tree) =
             let (label, _) = tree.node
             let (trees, extents) = List.unzip (List.map innerDesign tree.children)
             let positions = fitlist extents
-
-            let zippedTrees = List.zip trees positions
-            let ptrees = List.map moveTree zippedTrees
-
-            let zippedExtents = List.zip extents positions
-            let pextents = List.map moveExtent zippedExtents
-
+            let ptrees = List.map moveTree (List.zip trees positions)
+            let pextents = List.map moveExtent (List.zip extents positions)
             let flatExtents = List.fold (fun state item -> merge (state, item)) [] pextents
-
             let resultExtent = (0.0, 0.0) :: flatExtents
-
             let resulttree =
                 { node = (label, 0.0)
                   children = ptrees }
             (resulttree, resultExtent)
-        innerDesign (tree)
+        fst(innerDesign (tree))
