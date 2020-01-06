d7755288259d5918662acdbd438f09654907573d
Baizey <lassemoerk.mrk@gmail.com>
2020-01-06 14:43:21 +0100

    added  Tree to PostScript formatter

--- Driver.fs
+++ Driver.fs
@@ -1,14 +1,15 @@
 ﻿namespace Project1
 
 open AndrewKennedyTree
+open PostScript
 
 module Driver =
     [<EntryPoint>]
     let main argv =
         let testTree = Node("A", [
-          Node("B", [Node("C", []); Node("D", []); Node("E", [])]); 
-          Node("F", []); 
+          Node("B", [Node("D", []); Node("E", [])]); 
+          Node("F", [Node("F1", []); Node("F2", [])]); 
           Node("G", [Node("H", []); Node("I", []); Node("J", [])])])
         let posTree = designTree testTree
-        printfn "%A" posTree
+        printfn "%A" (postScriptString posTree)
         0
--- PostScript.fs
+++ PostScript.fs
@@ -3,32 +3,44 @@
 open AndrewKennedyTree
 open System.Text
 
-module PostScript = 
+module PostScript =
 
-  let postScriptString (tree: 'a PosTree) =
-    // Todo: Normalize
+    let postScriptString (tree: 'a PosTree) =
+        let output = StringBuilder("<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n")
+        output.AppendLine("1 1 scale").AppendLine("700 999 translate").AppendLine("newpath")
+              .AppendLine("/Times-Roman findfont 30 scalefont setfont") |> ignore
 
-    let output = new StringBuilder("<</PageSize[1400, 1000]/ImagingBBox null>> setpagedevice\n")
-    output.AppendLine("1 1 scale")
-          .AppendLine("700 999 translate")
-          .AppendLine("newpath")
-          .AppendLine("/Times-Romand findfont 10 scalefont setfont") |> ignore
 
-    
-    let draw (root: 'a PosTree) =
-      let rec drawInner (PosNode((label, pos), children): 'a PosTree) (x:float) (y:float) =
-        // Draw current node
-        
-        // Draw children
-        for c in children do
-          
-        // Moveto current node again
+        let factor = 100.0
+        let halfFactor = factor / 2.0
+        let textSize = 30.0
+        let halfTextSize = textSize / 2.0
 
-        //match children with
-        //| h :: hs -> drawInner h x y + 1
-          
-      drawInner tree 0.0 0.0
+        let rec draw (PosNode((label, pos), children): 'a PosTree) (x: float) (y: float) (isRoot: bool): int =
+            let rec drawInner (children: 'a PosTree list) (x: float) (y: float) =
+                match children with
+                | [] -> 0
+                | PosNode((label, pos), children) :: tail ->
+                    draw (PosNode((label, pos), children)) x y false |> ignore
+                    drawInner tail x y
+            match isRoot with
+            | true ->
+                output.AppendLine(sprintf "%d %d moveto" (int (x + pos * factor)) (int (y - factor)))
+                      .AppendLine(sprintf " (%s) dup stringwidth pop 2 div neg 0 rmoveto show" (label.ToString()))
+                |> ignore
+            | false ->
+                output.AppendLine(sprintf "%d %d moveto" (int x) (int (y - halfTextSize)))
+                      .AppendLine(sprintf "%d %d lineto" (int x) (int (y - halfFactor)))
+                      .AppendLine(sprintf "%d %d lineto" (int (x + pos * factor)) (int (y - halfFactor)))
+                      .AppendLine(sprintf "%d %d lineto" (int (x + pos * factor)) (int (y - factor + textSize)))
+                      .AppendLine(sprintf "%d %d moveto" (int (x + pos * factor)) (int (y - factor)))
+                      .AppendLine(sprintf " (%s) dup stringwidth pop 2 div neg 0 rmoveto show" (label.ToString()))
+                |> ignore
+            match children with
+            | [] -> 0
+            | _ ->
+                drawInner children (floor (x + pos * factor)) (floor (y - factor))
+        draw tree 0.0 0.0 true |> ignore
+        output.AppendLine("stroke").AppendLine("showpage").ToString()
 
-    0
-
-  //let postScriptFile = 
+//let postScriptFile =
